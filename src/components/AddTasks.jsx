794b3ab76e894124912f9014f77145ab18477e15
João Pedro França <161327584+Joao-Franca@users.noreply.github.com>
2024-10-24 23:02:56 -0300

    new_update

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,6 +1,10 @@
 function AddTasks (){
     return (
-        <h1></h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col ">
+            <input type="text" placeholder="Digite o título da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"></input>
+            <input type="text" placeholder="Digite a descrução da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"></input>
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md">Adicionar</button>
+        </div>
     );
 }
 
